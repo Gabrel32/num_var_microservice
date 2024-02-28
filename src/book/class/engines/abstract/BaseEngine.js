43dd93fa2aa4d6c6f9dd024c7ee942fe9ae56bdd
Gabriel Hernandez <Alegabo70@gmail.com>
2024-02-27 21:29:39 -0400

    integrando miguel base

--- src/book/class/engines/abstract/BaseEngine.js
+++ src/book/class/engines/abstract/BaseEngine.js
@@ -2,48 +2,49 @@ class BaseEngine {
     constructor() {
         this.def = {}
         this.htmlNode = null
+        this.timerInteraction = 0;
+        this.Timer = null
+        this.timerActive = false
+    }
+    
+    initTimer(node) {
+        /* agregar todos los eventos necesarios o que peudan tener el ejercicio */
+        this.htmlNode.addEventListener('mouseenter', this.iniciarTimer)
+        this.htmlNode.addEventListener('input', this.iniciarTimer)
+        this.htmlNode.addEventListener('mouseleave', ()=>{
+            this.detenerTimer()
+        })
+        this.htmlNode.addEventListener("click", (event) => {
+            if (event.target.matches(".btnV")) {
+                this.resetTimer()
+                this.detenerTimer()
+            } 
+        });
     }
 
-    timer(def, mode = true, reset = true) {
-        if (!def) { return; };
-        def.timeInteraction = def?.timeInteraction ?? 0;
-        if (def.statusValidate === undefined) {
-            def.statusValidate = false;
-        };
+    iniciarTimer = (cont = true) => {
 
-        if (!def.statusValidate) {
-            if (mode) {
-                if (!def.timerState) {
-
-                    def.timer = setInterval(function () {
-                        if (def.statusValidate) {
-                            clearInterval(def.timer);
-                            def.timeInteraction = 0;
-                            return;
-                        };
-                        def.timeInteraction++;
-                        if (def.debug) {
-                            console.log(def.timeInteraction);
-                        };
-                    }, 1000);
-
-                    def.timerState = true;
-                };
-            } else {
-                if (def.timer) {
-                    clearInterval(def.timer);
-                    def.timerState = false;
-                }
-                if (reset) {
-                    def.timeInteraction = 0;
-                    def.statusValidate = true;
-                };
-            };
-        };
+        if (this.timerActive) {
+            // console.log('se llama pero no se crea otro timer');
+            return
+        }
+        this.timerActive = true
+        this.Timer = setInterval(() => {
+            this.timerInteraction = this.timerInteraction + 1
+            console.log(this.timerInteraction);
+        }, 1000)
     }
 
-    addTimer() {
-        console.log('timer');
+    detenerTimer = () => {
+        this.timerActive = false
+        clearInterval(this.Timer);
+        this.Timer = null
+    }
+
+    resetTimer = () => {
+        console.log(`Tiempo transcurrido: ${this.timerInteraction} segundos;`);
+        clearInterval(this.Timer)
+        this.timerInteraction = 0
     }
 
 }
